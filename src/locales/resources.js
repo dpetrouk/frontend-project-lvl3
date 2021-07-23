1d471729a597a305a4ddba2fb755563780ce8986
Dmitry Petrouk <dpetrouk@gmail.com>
2021-07-23 21:11:07 +0300

    add more texts and en locale

--- src/locales/resources.js
+++ src/locales/resources.js
@@ -1,24 +1,67 @@
 const resources = {
   ru: {
     translation: {
-      errors: {
-        validation: {
-          url: 'Ссылка должна быть валидным URL', //'this must be a valid URL'
-          uniqueness: 'RSS уже существует', // duplication // 'RSS already exists'
+      pageTitle: 'RSS агрегатор',
+      pageDescription: 'Здесь можно читать RSS фиды',
+      form: {
+        btnAdd: 'Добавить',
+        labelForInput: 'RSS ссылка',
+        examples: 'Примеры',
+        feedback: {
+          success: 'RSS успешно загружен',
+          errors: {
+            validation: {
+              url: 'Ссылка должна быть валидным URL',
+              uniqueness: 'RSS уже существует',
+            },
+            network: 'Ошибка сети',
+            resource: 'Ресурс не содержит валидный RSS',
+          },
         },
-        network: 'Ошибка сети', // network 'Network Error'
-        resource: 'Ресурс не содержит валидный RSS',
-        
       },
-      feedback: {
-        success: 'RSS успешно загружен',
+      feeds: {
+        header: 'Фиды',
       },
-      interface: {
-        feeds: 'Фиды',
-        posts: 'Посты',
-        show: 'Просмотр',
-        open: 'Читать полностью',
-        close: 'Закрыть',
+      posts: {
+        header: 'Посты',
+        btnShow: 'Просмотр',
+      },
+      modal: {
+        btnOpen: 'Читать полностью',
+        btnClose: 'Закрыть',
+      },
+    },
+  },
+  en: {
+    translation: {
+      pageTitle: 'RSS agregator',
+      pageDescription: 'You can read RSS feeds here',
+      form: {
+        btnAdd: 'Add',
+        labelForInput: 'RSS link',
+        examples: 'Examples',
+        feedback: {
+          success: 'RSS is loaded',
+          errors: {
+            validation: {
+              url: 'This must be a valid URL',
+              uniqueness: 'RSS already exists',
+            },
+            network: 'Network Error',
+            resource: 'Resource doesn\'t contain valid RSS',
+          },
+        },
+      },
+      feeds: {
+        header: 'Feeds'
+      },
+      posts: {
+        header: 'Posts',
+        btnShow: 'Show',
+      },
+      modal: {
+        btnOpen: 'Read more',
+        btnClose: 'Close',
       },
     },
   },
