8bcf6681ec80facec1ad8ee7455d86cf9b23116e
Dmitry Petrouk <dpetrouk@gmail.com>
2021-07-28 12:43:33 +0300

    move texts to files, add en, organize texts

---
+++ src/locales/en.js
@@ -0,0 +1,34 @@
+export default {
+  translation: {
+    pageTitle: 'RSS agregator',
+    pageDescription: 'You can read RSS feeds here',
+    form: {
+      btnAdd: 'Add',
+      labelForInput: 'RSS link',
+      examples: 'Examples',
+      feedback: {
+        success: 'RSS is loaded',
+        errors: {
+          validation: {
+            required: 'This field should not be empty',
+            url: 'This must be a valid URL',
+            uniqueness: 'RSS already exists',
+          },
+          network: 'Network Error',
+          resource: 'Resource doesn\'t contain valid RSS',
+        },
+      },
+    },
+    feeds: {
+      header: 'Feeds',
+    },
+    posts: {
+      header: 'Posts',
+      btnShow: 'Show',
+    },
+    modal: {
+      btnOpen: 'Read more',
+      btnClose: 'Close',
+    },
+  },
+};
---
+++ src/locales/ru.js
@@ -0,0 +1,34 @@
+export default {
+  translation: {
+    pageTitle: 'RSS агрегатор',
+    pageDescription: 'Здесь можно читать RSS фиды',
+    form: {
+      btnAdd: 'Добавить',
+      labelForInput: 'RSS ссылка',
+      examples: 'Примеры',
+      feedback: {
+        success: 'RSS успешно загружен',
+        errors: {
+          validation: {
+            required: 'Не должно быть пустым',
+            url: 'Ссылка должна быть валидным URL',
+            uniqueness: 'RSS уже существует',
+          },
+          network: 'Ошибка сети',
+          resource: 'Ресурс не содержит валидный RSS',
+        },
+      },
+    },
+    feeds: {
+      header: 'Фиды',
+    },
+    posts: {
+      header: 'Посты',
+      btnShow: 'Просмотр',
+    },
+    modal: {
+      btnOpen: 'Читать полностью',
+      btnClose: 'Закрыть',
+    },
+  },
+};
